5981ec03db178beb0fccedd7000c0939db9a6f21
maulana <maulana@pasarpolis.com>
2018-12-01 22:23:14 +0700

    add search functionality

--- server/controllers/restaurants.js
+++ server/controllers/restaurants.js
@@ -1,4 +1,6 @@
 const Restaurants = require('../models/restaurants')
+const Sequelize = require('sequelize')
+const $ = Sequelize.Op
 
 const getRestaurants = async (req, res) => {
     let response
@@ -15,6 +17,30 @@ const getRestaurants = async (req, res) => {
     res.status(200).json(response)
 }
 
+const getRestaurantsByDate = async (req, res) => {
+    const {q} = req.query
+    if (q === '' || Object.values(req.query).length < 1) {
+        return res.status(400).json({ message: 'Query can not empty' })
+    }
+    let response 
+    try {
+        response = await Restaurants.findAll({
+            where: {
+                open: {[$.like]: '%' + q + '%'}
+            }
+        })
+    } catch (error) {
+        console.error(error.errors)
+        if (error.errors && error.errors.length > 0) {
+            const first = error.errors[0]
+            return res.status(400).json({ message: first.message })
+        }
+        return res.status(500).json({ message: error.message })
+    }
+    res.status(200).json(response)
+}
+
 module.exports = {
-    getRestaurants
+    getRestaurants,
+    getRestaurantsByDate
 }
--- server/routers/restaurants.js
+++ server/routers/restaurants.js
@@ -8,4 +8,10 @@ router.get(
     controller.getRestaurants
 )
 
+router.get(
+    '/find', 
+    checkAuth,
+    controller.getRestaurantsByDate
+)
+
 module.exports = router
